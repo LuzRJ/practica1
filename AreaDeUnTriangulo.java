51c6abd26d1b740a946c40ff0b36156695931eb0
LuzRJ <143624358+LuzRJ@users.noreply.github.com>
2023-09-08 08:50:30 -0600

    Add files via upload

---
+++ AreaDeUnTriangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area.de.un.triangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaDeUnTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double a=12;
+       double b=10;
+       double area=  b*a/2;
+        System.out.println("el area del triangulo es"+area);
+       
+    
+}}
+   
